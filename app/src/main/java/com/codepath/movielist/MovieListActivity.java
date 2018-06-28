bf3aa34adb6712db8f116e61177281485fcafcd4
tyu91 <tiffanyy9968@gmail.com>
2018-06-28 13:48:48 -0700

    Added code to get configuration and get list of currently playing movies

--- app/src/main/java/com/codepath/movielist/MovieListActivity.java
+++ app/src/main/java/com/codepath/movielist/MovieListActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.codepath.movielist.models.Movie;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -12,6 +13,8 @@ import com.loopj.android.http.RequestParams;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -32,13 +35,50 @@ public class MovieListActivity extends AppCompatActivity {
     String imageBaseUrl;
     //poster size to use when fetching imgs
     String posterSize;
+    //list of currently playing movies
+    ArrayList<Movie> movies;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+        movies = new ArrayList<>();
         client = new AsyncHttpClient();
         getConfiguration();
+        getNowPlaying();
+    }
+
+    private void getNowPlaying() {
+        //create the url
+        String url = API_BASE_URL + "/movie/now_playing";
+        //set up request parameters
+        RequestParams params = new RequestParams();
+        params.put(API__KEY_PARAM, getString(R.string.api_key)); //this is API key: always necessary!!!
+        //execute a GET request that expects a response from JSON object
+        client.get(url, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONObject images = response.getJSONObject("images");
+                    //get image base url
+                    imageBaseUrl = images.getString("secure_base_url");
+                    //get poster size
+                    JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
+                    //use the option at index 3 or w342 as fallback
+                    posterSize = posterSizeOptions.optString(3, "w342");
+                    Log.i(TAG, String.format("Loaded config w imageBaseUrl %s and posterSize %s", imageBaseUrl, posterSize));
+
+                } catch (JSONException e) {
+                    logError("Failed parsing config", e, true);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                logError("Failed to get configuration", throwable, true);
+            }
+        });
     }
 
     //get the config from API
